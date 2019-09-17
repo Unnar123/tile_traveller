b713f48d1d01f23f5577a9fc671cfb8daef49fd3
unnar123 <unnarj18@ru.is>
2019-09-17 12:07:32 +0000

    Final

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,93 +1,101 @@
 ''' Use x and y variables to track were the user is.
-    use functions to move the user where he chooses.
+    Use statemensts to move the user where he chooses.
     Print out where the user is, so he can move according to wich tile he is on.
     Winner tile is (3,1) and then Winner is printed
 '''
-''' 
-function for tile 3,3 and 2,2
-def upp_right(x,y):
 
-'''
+print("You can travel: (N)orth.")
 direction = input('Direction: ')
-x=3
-y=3
+x=1
+y=1
 
 while True:
     # (1,1)
     if x == 1 and y == 1:
-        if direction == "n" or direction == "N":
-            y += 1
-        else:
-            print("Not a valid direction!")
-        print("You can travel: (N)orth or (E)ast or (S)outh.")
+       if direction == "n" or direction == "N":
+           y += 1
+           print("You can travel: (N)orth or (E)ast or (S)outh.")
+       else:
+          print("Not a valid direction!")
+       direction =  input('Direction: ')
 
     # (1,2)
     if x == 1 and y == 2:
         if direction == "n" or direction == "N":
             y += 1
+            print('You can travel: (E)ast or (S)outh.')
         elif direction == "s" or direction == "S":
             y -= 1
+            print("You can travel: (N)orth.")
         elif direction == "e" or direction == "E":
             x += 1
+            print("You can travel: (S)outh or (W)est.")
         else:
             print("Not a valid direction!")
-        print
+        direction =  input('Direction: ')
     #(1,3)
     if x == 1 and y == 3:
         if direction == "e" or direction == "E":
             x += 1
+            print("You can travel: (E)ast or (W)est.")
         elif direction == "s" or direction == "S":
             y -= 1
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
         else: 
             print("Not a valid direction!")
+        direction =  input('Direction: ')
     # (2,1)
     if x == 2 and y == 1:
         if direction == "n" or direction == "N":
             y += 1
+            print("You can travel: (S)outh or (W)est.")
         else: 
             print("Not a valid direction!")
+        direction =  input('Direction: ')
     # (2,2)
     if x == 2 and y == 2:
         if direction == "w" or direction == "W":
             x -= 1
-        if direction == "s" or direction == "S":
-            y -= 1
+            print("You can travel: (N)orth or (E)ast or (S)outh.")
+        elif direction == "s" or direction == "S":
+            y-= 1
+            print("You can travel: (N)orth.")
         else:
             print("Not a valid direction!")
-
-
-
-
+        direction =  input('Direction: ')
     #Tile 2,3
     if x==2 and y==3:
         if direction == 'e' or direction == 'E':
             x+=1
+            print("You can travel: (S)outh or (W)est.")
         elif direction == 'w' or direction == 'W':
             x-=1
+            print("You can travel: (S)outh or (E)ast.")
         else: 
             print('Not a valid direction!')
+        direction =  input('Direction: ')
     #Tile 3,2
     if x==3 and y==2:
         if direction == 'n' or direction=='N':
-            x+=1
+            y+=1
+            print("You can travel: (S)outh or (W)est.")
         elif direction == 's' or direction == 'S':
-            y -=1
+            y-=1
+            break
         else: 
             print('Not a valid direction!')
-        
-    # Tiles 3,3 and 2,2
-    if x==3 and y==3 or x==2 and y==2:
-         if direction == 'e' or direction == 'E':
-             x -= 1
+        direction =  input('Direction: ')
+    # Tiles 3,3
+    if x==3 and y==3:
+         if direction == 'w' or direction == 'W':
+              x -= 1
+              print("You can travel: (E)ast or (W)est.")
          elif direction == 's' or direction == 'S':
              y-=1
+             print("You can travel: (N)orth or (S)outh.")
          else:
              print('Not a valid direction')
-
+         direction =  input('Direction: ')
     if x==3 and y==1:
-        break
-    print(x,y)    
-    direction =  input('Direction: ')
-    print("hallo")
-    
+        break    
 print('Victory!')
